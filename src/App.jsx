4c82c79533c5f1ab70cf4a9312feec2400530d05
Rifqi007 <130329176+Rifqi007@users.noreply.github.com>
2025-01-20 13:51:56 +0700

    Upload file code website profil ke repository

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashLink } from "react-router-hash-link";
+
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+
+import HomePage from "./pages/HomePage";
+
+function App() {
+  return (
+    <Router>
+      <div>
+        {/* Navbar */}
+        <Navbar/>
+        
+        {/* Content */}
+        <Routes>
+          <Route path="/" element={ <HomePage/> } />
+        </Routes>
+        <Footer/>
+      </div>
+    </Router>
+  )
+}
+
+export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,78 @@
+import { HashLink as Link } from "react-router-hash-link";
+
+import { useEffect, useState } from "react";
+
+const Navbar = () => {
+    const [show, setShow] = useState(false);
+    const [scroll, setScroll] = useState(false);
+
+    const handleClick = ()=>{
+        setShow(!show);
+        // console.log(show);
+    };
+
+    let menuActive = show ? "left-0" : "-left-full";
+
+    useEffect(() => {
+        window.addEventListener("scroll", () => {
+            if (window.scrollY > 5 ) {
+                // console.log()
+                setScroll(true);
+                setShow(false);
+            } else {
+                setScroll(false);
+            }
+        });
+    });
+
+    let scrollActive = scroll ? "py-6 bg-white shadow" : "py-4"
+  return (
+    <div className={`navbar fixed w-full transition-all ${scrollActive}`}>
+        <div className="container mx-auto px-4 ">
+            <div className="navbar-box flex items-center justify-between">
+                <div className="logo-">
+                    <h1 className="sm:text-2xl text-xl font-bold">RIFQI</h1>
+                </div>
+                <ul 
+                className={`flex lg:gap-12 md:static md:flex-row md:shadow-none md:bg-transparent md:w-auto md:h-full md:translate-y-0 md:text-black 
+                md:p-0 md:m-0 md:transition-none gap-8 fixed ${menuActive} top-1/2 -translate-y-1/2 flex-col px-8 py-6 rounded shadow-lg shadow-slate-300 
+                bg-sky-400 font-bold text-white transition-all`}
+                >
+                    <li className="flex items-center gap-3">
+                        <i className="ri-home-3-line text-3xl md:hidden block"></i>
+                        <Link to="#home" className="font-medium opacity-75">
+                            Beranda
+                        </Link>
+                    </li>
+                    <li className="flex items-center gap-3">
+                        <i className="ri-information-2-line text-3xl md:hidden block"></i>
+                        <Link to="#about" className="font-medium opacity-75">
+                            Tentang Kami
+                        </Link>
+                    </li>
+                    <li className="flex items-center gap-3">
+                        <i className="ri-customer-service-line text-3xl md:hidden block"></i>
+                        <Link to="#services" className="font-medium opacity-75">
+                            Layanan
+                        </Link>
+                    </li>
+                    <li className="flex items-center gap-3">
+                        <i className="ri-projector-line text-3xl md:hidden block"></i>
+                        <a href="#contact-us" className="font-medium opacity-75">
+                            Hubungi Kami
+                        </a>
+                    </li>
+                </ul>
+                <div className="social flex items-center gap-2">
+                    <Link to="#social" className="bg-sky-400 px-5 py-2 rounded-full text-white font-bold hover:bg-sky-500 transition-all">
+                        Social Media
+                    </Link>
+                    <i className="ri-menu-3-line text-3xl md:hidden block" onClick={handleClick}></i> 
+                </div>
+            </div>
+        </div>
+  </div>
+  );
+};
+
+export default Navbar;
